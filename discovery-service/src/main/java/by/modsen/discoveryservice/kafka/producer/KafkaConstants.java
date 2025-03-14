66d0c71fb98dd98800f4b31e1ab64322945063fd
Ruslan Alhava <enotland34@yandex.ru>
2025-03-15 01:46:12 +0300

    Monitoring finished (metrics, logs(request and response), traces, spans) for all services.

---
+++ discovery-service/src/main/java/by/modsen/discoveryservice/kafka/producer/KafkaConstants.java
@@ -0,0 +1,14 @@
+package by.modsen.discoveryservice.kafka.producer;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class KafkaConstants {
+
+    public static final String BOOTSTRAP_ADDRESS = "${spring.kafka.producer.bootstrap-servers}";
+    public static final int AMOUNT_OF_REPLICAS = 3;
+    public static final int AMOUNT_OF_PARTITIONS = 3;
+    public static final String LOGS_TOPIC_NAME = "logs";
+
+}
--- driver-service/src/main/java/by/modsen/driverservice/constants/ApplicationConstants.java
+++ driver-service/src/main/java/by/modsen/driverservice/constants/ApplicationConstants.java
@@ -10,5 +10,10 @@ public final class ApplicationConstants {
     public static final String PHONE_REGEX =
         "^\\+375(25\\d{3}\\d{2}\\d{2}|(29[1-9]\\d{2}\\d{2}\\d{2})|(33\\d{3}\\d{2}\\d{2})|(44\\d{3}\\d{2}\\d{2}))$";
     public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error occurred!";
+    public static final String REQUEST_LOGGING_MESSAGE =
+        "Request. Method: {}, URI: {}, Args -> {}";
+    public static final String RESPONSE_LOGGING_MESSAGE =
+        "Response. Method: {}, URI: {}, Args -> {} Time Taken: {} ms";
+    public static final String ERROR_SERIALIZING_JSON_MESSAGE = "Error serializing object to JSON";
 
 }
